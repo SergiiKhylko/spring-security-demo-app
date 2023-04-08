0ae84c260dafe555ed5c68aec0cd824d0e6f5362
Sergii <sergii.khylko@outlook.com>
2023-04-08 13:43:37 +0300

    #2 add inMemory authorities

--- src/main/java/com/ajkko/spring/security/demo/app/config/SecurityConfiguration.java
+++ src/main/java/com/ajkko/spring/security/demo/app/config/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package com.ajkko.spring.security.demo.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,23 +15,32 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests().anyRequest().permitAll();
-
-        http.authorizeHttpRequests().anyRequest().authenticated();
-        http.formLogin();
-        http.httpBasic();
+        http.authorizeHttpRequests()
+                .antMatchers("/admin").hasAuthority("ADMIN")
+                .antMatchers("/moder").hasAnyAuthority("ADMIN", "MODER")
+                .antMatchers("/user").hasAnyAuthority("ADMIN", "MODER", "USER")
+                .antMatchers("/hello").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("Sergii")
+                .withUser("admin")
                 .password(passwordEncoder().encode("pass"))
-                .authorities("USER", "ADMIN")
+                .authorities("ADMIN", "MODER", "USER")
                 .and()
-                .withUser("Vasia")
+                .withUser("user")
                 .password(passwordEncoder().encode("pass"))
-                .authorities("GUEST");
+                .authorities("USER")
+                .and()
+                .withUser("moder")
+                .password(passwordEncoder().encode("pass"))
+                .authorities("MODER", "USER");
     }
 
     @Bean
--- src/main/java/com/ajkko/spring/security/demo/app/controller/MainController.java
+++ src/main/java/com/ajkko/spring/security/demo/app/controller/MainController.java
@@ -1,7 +1,6 @@
 package com.ajkko.spring.security.demo.app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,15 +11,19 @@ public class MainController {
         return "<h2>Hello everyone!</h2>";
     }
 
+    @GetMapping("/admin")
+    public String admin() {
+        return "<h2>Hello Admin!</h2>";
+    }
 
-//    @GetMapping("/user")
-//    public String user() {
-//        return "<h2>Hello, User!</h2>";
-//    }
-//
-//    @GetMapping("/admin")
-//    public String admin() {
-//        return "<h2>Hello, Admin!</h2>";
-//    }
+    @GetMapping("/user")
+    public String user() {
+        return "<h2>Hello User!</h2>";
+    }
+
+    @GetMapping("/moder")
+    public String moder() {
+        return "<h2>Hello Moderator!</h2>";
+    }
 
 }
